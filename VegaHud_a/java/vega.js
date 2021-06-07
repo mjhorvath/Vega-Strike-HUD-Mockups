a373fdb8184ccdf88df0ee2f1635c9a11fd3ffd4
Michael Horvath <17457030+mjhorvath@users.noreply.github.com>
2021-06-07 10:45:28 -0400

    Updates

--- VegaHud_a/java/vega.js
+++ VegaHud_a/java/vega.js
@@ -1,3 +1,11 @@
+// Probably should require that a VDU be enabled before making a selection. This will definately be the case for keyboard input. However, I don't think this should be required when using a mouse.
+// Should make it so that the first selection within each tab is highlighted by default. (keyboard input only?)
+// Still need to show current date & time, location, and FPS.
+// Need to code the actions.
+// Need to figure out the order of actions for VDU 2, and whether they're done correctly.
+// Note: the 'display' property of a container is inherited. Should test this further.
+// Note: filter opacity is cumulative in Mozilla, but not in IE. By moving the individual panes outside the VDUs, the behavior will be the same in both programs.
+
 //------------------------------------------------------------------------------Global Variables
 
 // numerical
@@ -211,7 +219,6 @@ function vduToggle(ev)
 {
 	var pass = false
 	var oldVdu = currentVdu
-	// coordinates should really be relative to the parent element
 	var coo_lft = ev.pageX
 	var coo_top = ev.pageY
 	// buggy in strict mode IE8
